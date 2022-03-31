444f64ee6568cdab1304e0c4daf83abf6e063afc
cajr11 <caondo@yahoo.co.uk>
2022-03-31 08:54:12 -0600

    services section responsiveness implemented

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,11 +5,10 @@ import classes from "./Services.module.css"
 const Services = () => {
   return (
     <section className={classes.container}>
+         {/* Heading */}
+         <h2 className={classes.heading}>Why Us</h2>
+
         <div className={classes.inner}>
-
-            {/* Heading */}
-            <h2 className={classes.heading}>Why Us</h2>
-
             {/* Services */}
             <ProvidedService service="Home Delivery" />
             <ProvidedService service="Tea Personalization" />
